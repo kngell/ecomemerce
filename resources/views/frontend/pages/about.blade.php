ba87931db16666f5c26540abc766c02b89c76523
kngeell <admin@kngell.com>
2021-02-03 23:01:45 +0100

    explication asset url

--- resources/views/frontend/pages/about.blade.php
+++ resources/views/frontend/pages/about.blade.php
@@ -3,7 +3,7 @@
 @section('title','About Us | ' . config('app.name'))
 <!-- Content -->
 @section('content')
-<img src="/img/consulting.png" alt="Daniel AKONO">
+<img src="{{ asset('img/consulting.png') }}" alt="Daniel AKONO">
 <p>Built with &hearts;by les teachers du net</p>
 <p><a href="{{route('app_home')}}">Revenir à la page d'accueil</a></p>
 @endsection
